bca71d51813217310232b088697dfe6d95bf25b7
Vara <artemiivoropaev@gmail.com>
2026-02-12 13:46:15 +0700

    add test for getting petId NonexistentPet

--- src/test/java/tests/TestPet.java
+++ src/test/java/tests/TestPet.java
@@ -72,4 +72,30 @@ public class TestPet {
                         "Код ответа не совпал с ожидаемым. Ответ: " + responseBody)
         );
     }
+
+    @Test
+    @Feature("Pet")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("artemy voropaev")
+    public void testGetIdNonexistentPet() {
+
+        Response response = step("Отправить GET запрос на получение ID несуществующего питомца", () ->
+                given()
+                        .contentType(ContentType.JSON)
+                        .header("Accept", "application/json")
+                        .when()
+                        .get(BASE_URL + "/pet/9999"));
+
+        String responseBody = response.getBody().asString();
+
+        step("Проверить что статус-код ответа == 404", () ->
+                assertEquals(404, response.getStatusCode(),
+                        "Код ответа не совпал с ожидаемым. Ответ: " + responseBody)
+        );
+
+        step("Проверить что текст ответа 'Pet not found'", () ->
+                assertEquals("Pet not found", responseBody,
+                        "Код ответа не совпал с ожидаемым. Ответ: " + responseBody)
+        );
+    }
 }
